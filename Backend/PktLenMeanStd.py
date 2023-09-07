7ea530a7d9f6b66f83b05950665c604d64823b11
Elshaday1 <elshadaydereje7@gmail.com>
2023-09-07 13:18:52 +0300

    ...

--- Backend/PktLenMeanStd.py
+++ Backend/PktLenMeanStd.py
@@ -1,44 +1,29 @@
 import pyshark
 import statistics
 
-
 capture = pyshark.LiveCapture(interface='Ethernet')
 is_capture_active = True
 
-def calculate_mean_packet_length(interval):
-    packet_count = 0
-    total_length = 0
+def calculate_backward_packet_size(interval):
+    backward_packet_sizes = []
 
     for packet in capture:
-        packet_count += 1
-        total_length += int(packet.length)
+        if 'ack' in packet:
+            backward_packet_sizes.append(int(packet.length))
 
-        if packet_count % interval == 0:
-            mean_length = total_length / interval
-            total_length = 0
-            return mean_length
+        if len(backward_packet_sizes) == interval:
+            backward_size = sum(backward_packet_sizes)
+            backward_packet_sizes = []
+            return backward_size
 
+        if not is_capture_active:
+            break
 
-def calculate_packet_length_std_dev(interval):
-    packet_lengths = []
+    return sum(backward_packet_sizes)
 
-    for packet in capture:
-        packet_lengths.append(int(packet.length))
+backward_size = calculate_backward_packet_size(2)
+print("Size of Packets in the Backward Direction:", backward_size)
 
-        if len(packet_lengths) == interval:
-            std_dev = statistics.stdev(packet_lengths)
-            packet_lengths = []
-            return std_dev
-
-
-# mean_length = calculate_mean_packet_length(20)
-# std_dev = calculate_packet_length_std_dev(20)
-
-# print("Mean Packet Length:", mean_length)
-# print("Packet Length Standard Deviation:", std_dev)
-
-while True:
-    mean_length = calculate_mean_packet_length(20)
-    std_dev = calculate_packet_length_std_dev(20)
-    print("Mean Packet Length:", mean_length)
-    print("Packet Length Standard Deviation:", std_dev)
+while is_capture_active:
+    backward_size = calculate_backward_packet_size(2)
+    print("Size of Packets in the Backward Direction:", backward_size)
