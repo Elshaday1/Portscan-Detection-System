354569928711f5caf076d14ab2ba87946e6a6f87
Elshaday1 <elshadaydereje7@gmail.com>
2023-09-22 10:01:33 +0300

    ...

--- Backend/buffer.py
+++ Backend/buffer.py
@@ -1,17 +1,13 @@
-# Initialize an empty list to act as the buffer memory
 buffer_memory = []
 
-
-def add_to_buffer(number):
+# Define a function to add a number to the buffer memory
+def add_number_to_buffer(number):
     buffer_memory.append(number)
-    
-    # If the buffer memory exceeds three elements, remove the oldest number
-    if len(buffer_memory) > 3:
-        buffer_memory.pop(0)
 
+# Add numbers to the buffer memory
+add_number_to_buffer(5)
+add_number_to_buffer(10)
+add_number_to_buffer(15)
 
-add_to_buffer(5)
-add_to_buffer(10)
-add_to_buffer(15)
-
+# Print the buffer memory
 print(buffer_memory)
