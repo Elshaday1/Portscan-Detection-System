f8b138f9472867136d329058a613da279963ad35
Elshaday1 <elshadaydereje7@gmail.com>
2023-09-25 16:38:01 +0300

    ..

---
+++ Backend/CICclasses/sniffer.py
@@ -0,0 +1,79 @@
+import argparse
+
+from scapy.sendrecv import AsyncSniffer
+
+from flow_session import generate_session_class
+
+
+def create_sniffer( input_interface, output_mode, output_file, url_model=None
+):
+    # assert (input_interface is None)
+
+    NewFlowSession = generate_session_class(output_mode, output_file, url_model)
+
+    return AsyncSniffer(
+            iface=input_interface,
+            filter="ip and (tcp or udp)",
+            prn=None,
+            session=NewFlowSession,
+            store=False,
+        )
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    input_group = parser.add_mutually_exclusive_group(required=True)
+    input_group.add_argument(
+        "-i",
+        "--interface",
+        action="store",
+        dest="input_interface",
+        help="capture online data from INPUT_INTERFACE",
+    )
+
+    output_group = parser.add_mutually_exclusive_group(required=False)
+    output_group.add_argument(
+        "-c",
+        "--csv",
+        "--flow",
+        action="store_const",
+        const="flow",
+        dest="output_mode",
+        help="output flows as csv",
+    )
+
+    url_model = parser.add_mutually_exclusive_group(required=False)
+    url_model.add_argument(
+        "-u",
+        "--url",
+        action="store",
+        dest="url_model",
+        help="URL endpoint for send to Machine Learning Model. e.g http://0.0.0.0:80/prediction",
+    )
+
+    parser.add_argument(
+        "output",
+        help="output file name (in flow mode) or directory (in sequence mode)",
+    )
+
+    args = parser.parse_args()
+
+    sniffer = create_sniffer(
+        args.input_interface,
+        args.output_mode,
+        args.output,
+        args.url_model,
+    )
+    sniffer.start()
+
+    try:
+        sniffer.join()
+    except KeyboardInterrupt:
+        sniffer.stop()
+    finally:
+        sniffer.join()
+
+
+if __name__ == "__main__":
+    main()
