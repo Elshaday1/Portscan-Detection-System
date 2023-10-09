a651fc03ecf8e434d9c9ec64173236126a172e96
Elshaday1 <elshadaydereje7@gmail.com>
2023-10-09 17:01:50 +0300

    ..

--- Backend/auto_save.py
+++ Backend/auto_save.py
@@ -2,13 +2,12 @@
 # import time
 # from watchdog.observers import Observer
 # from watchdog.events import FileSystemEventHandler
-# from pynput import keyboard
 
-# # Define the directory and file to monitor
-# directory = 'C:\\Users\\Hp\Desktop\\CIC'
+
+# directory = 'C:\\Users\\Hp\\Desktop\\CIC'
 # filename = 'packet.csv'
 # file_path = os.path.join(directory, filename)
-# backup_filename = 'backup.csv'
+# backup_filename = 'Book1.csv'
 # backup_file_path = os.path.join(directory, backup_filename)
 
 # # Create a custom event handler to detect file modifications
@@ -20,24 +19,25 @@
 
 # # Function to save the new rows to the backup file
 # def save_new_rows():
+#     global num_existing_rows
 #     try:
 #         # Read the new rows from the original file
 #         with open(file_path, 'r') as file:
 #             rows = file.readlines()
 
+#         # Get the new rows starting from the last known position
+#         new_rows = rows[num_existing_rows:]
+
 #         # Append the new rows to the backup file
 #         with open(backup_file_path, 'a') as backup_file:
-#             new_rows = rows[len(existing_rows):]  # Only append the new rows
 #             backup_file.writelines(new_rows)
+
+#         # Update the number of existing rows
+#         num_existing_rows += len(new_rows)
 
 #         print("New rows saved successfully to the backup file.")
 #     except IOError:
 #         print("An error occurred while saving the new rows.")
-
-# # Function to handle keyboard input
-# def on_press(key):
-#     if key == keyboard.Key.enter:
-#         save_new_rows()
 
 # # Create an observer and attach the event handler
 # event_handler = FileModifiedHandler()
@@ -48,85 +48,52 @@
 # # Read the existing rows from the backup file
 # with open(backup_file_path, 'r') as backup_file:
 #     existing_rows = backup_file.readlines()
-
-# # Start monitoring the keyboard input
-# listener = keyboard.Listener(on_press=on_press)
-# listener.start()
+#     num_existing_rows = len(existing_rows)
 
 # try:
 #     while True:
+#         # Read the new rows from the original file
+#         with open(file_path, 'r') as file:
+#             rows = file.readlines()
+
+#         # Check if there are new rows to save
+#         if len(rows) > num_existing_rows:
+#             # Get the new rows starting from the last known position
+#             new_rows = rows[num_existing_rows:]
+
+#             # Append the new rows to the backup file
+#             with open(backup_file_path, 'a') as backup_file:
+#                 backup_file.writelines(new_rows)
+
+#             # Update the number of existing rows
+#             num_existing_rows += len(new_rows)
+
+#             print("New rows saved successfully to the backup file.")
+
 #         time.sleep(1)
 # except KeyboardInterrupt:
 #     observer.stop()
 
 # observer.join()
-# listener.stop()
-import os
-import time
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-from pynput import keyboard
+import csv
+import shutil
 
+def backup_csv_file(source_file, backup_file):
+    # Read the new rows from the source CSV file
+    with open(source_file, 'r', newline='') as source_csv:
+        reader = csv.reader(source_csv)
+        new_rows = list(reader)  # Read all rows
 
-directory = 'C:\\Users\\Hp\Desktop\\CIC'
-filename = 'packet.csv'
-file_path = os.path.join(directory, filename)
-backup_filename = 'Book1.csv'
-backup_file_path = os.path.join(directory, backup_filename)
+    # Append the new rows to the backup CSV file
+    with open(backup_file, 'a', newline='') as backup_csv:
+        writer = csv.writer(backup_csv)
+        writer.writerows(new_rows)
 
-# Create a custom event handler to detect file modifications
-class FileModifiedHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        if event.src_path == file_path:
-            print(f"Detected changes in '{filename}'. Saving the new rows to '{backup_filename}'...")
-            save_new_rows()
+    print(f"New rows from '{source_file}' appended to '{backup_file}'.")
 
-# Function to save the new rows to the backup file
-def save_new_rows():
-    try:
-        # Read the new rows from the original file
-        with open(file_path, 'r') as file:
-            rows = file.readlines()
-        
-        # Check if there are new rows to save
-        if len(rows) > num_existing_rows:
-            new_rows = rows[num_existing_rows:]  # Get the new rows
+# Specify the source and backup file paths
+source_file_path = 'C:\\Users\\Hp\\Desktop\\CIC\\packet.csv'
+backup_file_path = 'C:\\Users\\Hp\\Desktop\\CIC\\backup.csv'
 
-            # Append the new rows to the backup file
-            with open(backup_file_path, 'a') as backup_file:
-                backup_file.writelines(new_rows)
-
-            print("New rows saved successfully to the backup file.")
-        else:
-            print("No new rows to save.")
-    except IOError:
-        print("An error occurred while saving the new rows.")
-
-# Function to handle keyboard input
-def on_press(key):
-    if key == keyboard.Key.enter:
-        save_new_rows()
-
-# Create an observer and attach the event handler
-event_handler = FileModifiedHandler()
-observer = Observer()
-observer.schedule(event_handler, path=directory, recursive=False)
-observer.start()
-
-# Read the existing rows from the backup file
-with open(backup_file_path, 'r') as backup_file:
-    existing_rows = backup_file.readlines()
-    num_existing_rows = len(existing_rows)
-
-# Start monitoring the keyboard input
-listener = keyboard.Listener(on_press=on_press)
-listener.start()
-
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    observer.stop()
-
-observer.join()
-listener.stop()
+# Call the backup_csv_file function with the file paths
+backup_csv_file(source_file_path, backup_file_path)
--- Backend/startup.py
+++ Backend/startup.py
@@ -1,21 +1,7 @@
 import os
 import datetime
 import subprocess
-# from threading import Lock
-# from flask import Flask, render_template, session
-# from flask_socketio import SocketIO, emit
-# import test
-
-# async_mode = None
-# app = Flask(__name__)
-
-# socketio = SocketIO(app, async_mode=async_mode)
-# thread = None
-# thread_lock = Lock()
-
-
-# CSVFILEPATH = "C:/Users/Hp/Desktop/CICFlowmeter/bin/data/daily"
-# csvfilename = ""
+from subprocess import Popen
 
 def startup():
     # Check network flow csv file if it exists, if not create one.
@@ -27,30 +13,23 @@ def startup():
     # If network flow csv file does not exist, create a new one
     if isFileExist == False:
         file = open(os.path.join(r'C:\Users\Hp\Desktop\CICFlowmeter\bin\data\daily', csvfilename), 'w')
+        print("------daily file opened")
         file.close()
-    # Start CICFlowMeter
-    # command = "cd"
-#     Popen(['bash',os.path.join(curdirname,r"CICFlowMeter-4.0/bin/CICFlowMeter")], stdout=subprocess.PIPE)
+  
+  
+    directory_path = r'C:\Users\Hp\Desktop\CICFlowmeter\bin'
+    os.chdir(directory_path)
 
-    sub1 = subprocess.Popen(["C:/Users/Hp/Desktop/CICFlowmeter/bin//cfm.bat"], stdout=subprocess.PIPE)
-    sub2 = subprocess.Popen(["C:/Users/Hp/Desktop/CICFlowmeter/bin//CICFlowMeter.bat"], stdout=subprocess.PIPE)
+    
+    # sub1 = subprocess.Popen(["C:/Users/Hp/Desktop/CICFlowmeter/bin//cfm.bat"], stdout=subprocess.PIPE)
+    sub2 = subprocess.Popen(["C:/Users/Hp/Desktop/CICFlowmeter/bin//CICFlowMeter.bat"])
     # sub2 = subprocess.Popen("./CICFlowMeter.bat", stdin=sub1.stdout, stdout=subprocess.PIPE)
     # dropped = []
-    sub1.communicate()
-    sub2.communicate()
-# runIDS = exec(open('test.py').read())
-# @app.route('/')
-# def index():
-#     return render_template('index.html', async_mode=socketio.async_mode)
-
-
-# @socketio.event 
-# def connect():
-#     global thread
-#     with thread_lock:
-#         if thread is None:
-#             thread = socketio.start_background_task(runIDS)
-#     emit('my_response', {'data': 'Connected', 'count': 0})
+    # sub1.communicate()
+    # print("------cfm.bat opened")
+    print("------Current working directory:", os.getcwd())
+    # print("------Flowmeter opened")
+    # sub2.communicate()
+    # print("------Flowmeter closed")
 
 startup()
-# socketio.run(app)
