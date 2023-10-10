caff6242cc164698cf1f5f154e52faec31593b8d
Elshaday1 <elshadaydereje7@gmail.com>
2023-10-10 15:07:26 +0300

    ...

--- Backend/startup.py
+++ Backend/startup.py
@@ -7,7 +7,7 @@ def startup():
     # Check network flow csv file if it exists, if not create one.
     curdirname = os.getcwd() # current working directory
     # Generates a filename of the format 'YYYY-MM-DD_Flow.csv'
-    global csvfilename
+    global csvfilenam
     csvfilename = "%s_Flow.csv" % (datetime.datetime.today().strftime('%Y-%m-%d'))
     isFileExist = os.path.exists(os.path.join(r'C:\Users\Hp\Desktop\CICFlowmeter\bin\data\daily', csvfilename))
     # If network flow csv file does not exist, create a new one
